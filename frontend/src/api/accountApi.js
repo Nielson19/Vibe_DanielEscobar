658cf3ad39958c98913b0ff749e9c68c4e64b020
Niel <danielescobar492@gmail.com>
2026-03-26 19:24:14 -0400

    changes on routes for mongo

--- frontend/src/api/accountApi.js
+++ frontend/src/api/accountApi.js
@@ -1,11 +1,18 @@
-export const addTransaction = (accountId, amount, type) => axios.post('/transactions', { account_id: accountId, amount, type });
-export const getTransactionsByAccountId = (accountId) => axios.get(`/transactions/account/${accountId}`);
-// accountApi.js
-// Account-related API calls
 import axios from './axios';
 
+export const addTransaction = (accountId, amount, type) =>
+  axios.post(`/transactions/account/${accountId}`, { amount, type });
+
+export const getTransactionsByAccountId = (accountId) =>
+  axios.get(`/transactions/account/${accountId}`);
+
 export const getAccounts = () => axios.get('/accounts');
+
 export const getAccountsById = (id) => axios.get(`/accounts/user/${id}`);
+
 export const getUserById = (id) => axios.get(`/users/${id}`);
-export const addAccount = (userId, balance, type) => axios.post('/accounts', { user_id: userId, balance, account_type: type });
+
+export const addAccount = (userId, balance, type) =>
+  axios.post(`/accounts/user/${userId}`, { balance, account_type: type });
+
 export const removeAccount = (accountId) => axios.delete(`/accounts/${accountId}`);
